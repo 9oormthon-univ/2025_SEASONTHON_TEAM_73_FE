e953675581b2cafc38fbcfc0dcc8ffc3047a41a6
cho4u4o <cho4u4o@gmail.com>
2025-08-30 15:55:03 +0900

    feat : add font plugin

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "SUIT-Regular": require("../assets/fonts/SUIT-Regular.ttf"),
+    "SUIT-Medium": require("../assets/fonts/SUIT-Medium.ttf"),
+    "SUIT-SemiBold": require("../assets/fonts/SUIT-SemiBold.ttf"),
+    "SUIT-Bold": require("../assets/fonts/SUIT-Bold.ttf"),
+    "SUIT-Light": require("../assets/fonts/SUIT-Light.ttf"),
+    "SUIT-Thin": require("../assets/fonts/SUIT-Thin.ttf"),
+    "SUIT-ExtraLight": require("../assets/fonts/SUIT-ExtraLight.ttf"),
+    "SUIT-ExtraBold": require("../assets/fonts/SUIT-ExtraBold.ttf"),
+    "SUIT-Heavy": require("../assets/fonts/SUIT-Heavy.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack />;
 }
