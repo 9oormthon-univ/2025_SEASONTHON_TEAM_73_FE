6dea2913ee8f0aa92a388b6376b7b7ed505eb76c
ehy0414 <djagusdyd78@gmail.com>
2025-09-16 15:40:24 +0900

    feat: 프로필 이미지 업로드 기능 변경

--- widgets/profile-edit/ProfileHeader.tsx
+++ widgets/profile-edit/ProfileHeader.tsx
@@ -1,23 +1,22 @@
 import { COLORS, FONT_SIZE, FONTS } from '@/shared/styles';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { ProfileImage } from './ProfileImage';
 
 interface ProfileHeaderProps {
-  profileImage?: string;
-  onPressImage?: () => void;
+  profileImage: string | null; // 선택된 이미지 URL
+  onPressImage: () => void;    // 이미지 클릭 시 호출
 }
 
 export const ProfileHeader: React.FC<ProfileHeaderProps> = ({ profileImage, onPressImage }) => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={onPressImage}>
-        <ProfileImage
-          source={profileImage ? { uri: profileImage } : undefined}
-          showEdit={true}
-          size={80}
-        />
-      </TouchableOpacity>
+      <ProfileImage
+        source={profileImage ? { uri: profileImage } : undefined}
+        size={80}
+        showEdit={true}
+        onPress={onPressImage} // 클릭 시 이벤트 전달
+      />
       <Text style={styles.description}>
         책상, 침대 같은 생활 성향이 드러나는 사진으로 올리면 매칭에 도움이 돼요.
       </Text>
